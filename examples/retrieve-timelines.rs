cfd9611b93b5903d49a20f25ec93a098925ba266
Christian FOUCHER <christian.foucher@gmail.com>
2018-03-28 22:23:07 +0200

    Apply cargo fmt

--- examples/retrieve-timelines.rs
+++ examples/retrieve-timelines.rs
@@ -7,8 +7,7 @@ use horaire::source::ratp::ratp;
 use horaire::source::sncf::sncf;
 
 fn main() {
-    display_time_lines(transilien("PSL").unwrap_or( Vec::<TimeLine>::new()).iter());
+    display_time_lines(transilien("PSL").unwrap_or(Vec::<TimeLine>::new()).iter());
     display_time_lines(ratp("A", "Auber").unwrap_or(Vec::<TimeLine>::new()).iter());
     display_time_lines(sncf("PSL", true).unwrap_or(Vec::<TimeLine>::new()).iter());
 }
-
